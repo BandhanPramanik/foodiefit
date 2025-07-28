c5a1f38e703c1056236d1563315a805fbe73b18e
Bandhan Pramanik <bandhanpramanik06.foss@gmail.com>
2025-07-29 03:03:52 +0530

    Completed templating

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/1337")
+def test_lol():
+    return render_template("leet.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
